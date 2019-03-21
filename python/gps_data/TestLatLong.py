e5a27c77a4b410a86620db902bf287f6dd05760d
USVNick <magnann@mcmaster.ca>
2019-03-21 17:28:43 -0400

    HTML and python files for GPS.

---
+++ python/gps_data/TestLatLong.py
@@ -0,0 +1,6 @@
+import gmaps
+import gmaps.datasets
+gmaps.configure(api_key="AIzaSyDX4lmagliVDe24TwoK3JPiiCYp_NxdE7s")
+
+fig = gmaps.Map()
+fig
---
+++ python/gps_data/setGPS.py
@@ -0,0 +1,10 @@
+import os
+import googlemaps
+import gmaps
+import gmaps.datasets
+import pandas
+gmaps.configure(api_key='AIzaSyDX4lmagliVDe24TwoK3JPiiCYp_NxdE7s')
+
+
+fig = gmaps.figure()
+fig.show()
