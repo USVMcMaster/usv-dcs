289cb24354848be6cf7aa3d3cdcab88caf65b7f1
rohit <rawatr@mcmaster.ca>
2019-04-06 19:43:19 -0400

    Updated arduino code to relay input from python to motors

--- arduino/ser2out/ser2out.ino
+++ arduino/ser2out/ser2out.ino
@@ -1,29 +1,55 @@
-//#include <Servo.h>
-//Servo myservo;
-//byte servoPin = 9;
+#include <Servo.h>
+Servo servo;
+byte servoPin = 9;
 
 void setup()
 {
   Serial.begin(9600);
-  //servo.attach(servoPin);
+  servo.attach(servoPin);
 
-  //servo.writeMicroseconds(1500); //1500 is the stop signal for the ESC
-  //delay(2500) //Wait for ESC to recognize signal
+  servo.writeMicroseconds(1500); //1500 is the stop signal for the ESC
+  delay(5000); //Wait for ESC to recognize signal
+
+  Serial.println("Ready");
 }
 
 void loop()
 {
-  String data="";
+  String data;
+
+  int commandIndex;
+  String command;
+
+  int valueIndex;
+  float value;  
+
+  int signalValue;
+  
   while (Serial.available())
   {
     if(Serial.available() > 0)
     {
       data = Serial.readStringUntil('\r');
-      Serial.println(data);
 
-      //TODO:
-      //Parse signal from serial and write to int variable (call it signal)
-      //Then do: servo.writeMicroseconds(signal)
+      commandIndex = data.indexOf(',');
+      command = data.substring(0, commandIndex);
+
+      valueIndex = data.indexOf(':', commandIndex+1);
+      value = (data.substring(commandIndex+1,valueIndex)).toFloat();
+      
+      
+//      Serial.print("Command:");
+//      Serial.print(command);
+//      
+//      Serial.print(" Value:");
+//      Serial.println(value);
+          
+      //1700; // Set signal value, which should be between 1100 and 1900
+      signalValue = (((1900-1500)*(value - 0))/(1-0)) + 1500;
+
+      //Serial.print("Signal Value");
+      Serial.println(signalValue);
+      servo.writeMicroseconds(signalValue); // Send signal to ESC.
     }    
   }
 }
