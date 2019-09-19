2c4f87fa5e50f9ee5d916290d68ace7474212f1d
usvmac <usvmcmaster@gmail.com>
2019-09-19 11:51:39 -0400

    WIP

--- python/comm/client.py
+++ python/comm/client.py
@@ -1,6 +1,6 @@
 from socket import socket, AF_INET, SOCK_STREAM
 
-HOST = 'localhost'  # The server's hostname or IP address; post deployment, use laptop ip
+HOST = '10.42.0.1'  # The server's hostname or IP address; post deployment, use laptop ip
 PORT = 5000        # The port used by the server
 
 with socket(AF_INET, SOCK_STREAM) as s:
--- python/comm/server.py
+++ python/comm/server.py
@@ -1,17 +1,16 @@
-from socket import socket, AF_INET, SOCK_STREAM
+import socket
 import controller.controller_input as ci            # use for full rollout
-from inputs import get_gamepad
+
 # SOCK_STREAM is for TCP. Alternatively use import SOCK_DGRAM for UDP
 
-
 HOST = 'localhost'      # 127.0.0.1
-PORT = 5000             # Arbritary port > 1024
+PORT = 5005             # Arbritary port > 1024
 
 
-with socket(AF_INET, SOCK_STREAM) as s:     # Auto close socket, no need to use s.close
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:     # Auto close socket, no need to use s.close
     
     s.bind((HOST, PORT))                    # AF_INET (IPv4)
-    s.listen()                              # Monitor socket
+    s.listen(1)                             # Monitor socket
 
     conn, addr = s.accept()                 # Blocking while waiting for connection. 
                                             # Returns socket object on connection
@@ -20,8 +19,7 @@ with socket(AF_INET, SOCK_STREAM) as s:     # Auto close socket, no need to use 
         print('Connected by', addr)
 
         while True:
-            # ci.get_inputs()
             data = conn.recv(1024)
             if not data:
                 break
-            conn.sendall(data)
+            print("recieved data:", data)
