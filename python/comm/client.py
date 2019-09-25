db6e2cbd1abbfae19213ae07b02be1e30e1dae0d
usvmac <usvmcmaster@gmail.com>
2019-09-24 22:28:45 -0400

    working client test

--- python/comm/client.py
+++ python/comm/client.py
@@ -1,6 +1,6 @@
 from socket import socket, AF_INET, SOCK_STREAM
 
-HOST = 'localhost'  # The server's hostname or IP address; post deployment, use laptop ip
+HOST = '192.168.100.180'  # The server's hostname or IP address; post deployment, use laptop ip
 PORT = 5005        # The port used by the server
 
 with socket(AF_INET, SOCK_STREAM) as s:
