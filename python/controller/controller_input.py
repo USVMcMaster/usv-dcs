1c183cf906c6c169b9e96f1549d0be8b78fab458
rohit <rawatr@mcmaster.ca>
2019-10-02 00:52:09 -0400

    Changed file to simple function to parse controlled data.

--- python/controller/controller_input.py
+++ python/controller/controller_input.py
@@ -1,21 +1,18 @@
 from inputs import get_gamepad
 
-# TODO: Convert script to class to allow to be interfaced with motors
-
-
-# class Controller:
-    # def __init__(self):
-    #     self.events = get_gamepad()
-
-
 def get_inputs():
-    while 1:
+
         events = get_gamepad()
 
         for event in events:
-            print(event.ev_type, event.code, event.state)
+            data = event.ev_type, event.code, event.state
+
+        return data
 
 if __name__ == "__main__":
-    # c = Controller()
-    # c.get_inputs()
-    get_inputs()
+
+    try:
+        while True:
+            print(get_inputs())
+    except KeyboardInterrupt:
+        pass
