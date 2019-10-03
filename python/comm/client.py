23d7d53e62b98d9c456d00f76f25127dd30113a8
usvmac <usvmcmaster@gmail.com>
2019-10-03 12:02:10 -0400

    client splits input data to event and state

--- python/comm/client.py
+++ python/comm/client.py
@@ -1,20 +1,33 @@
 from socket import socket, AF_INET, SOCK_STREAM
-
-HOST = '192.168.100.180'  # The server's hostname or IP address; post deployment, use laptop ip
+# import controller.data_serializer as ds
+# Socket config
+HOST = '172.17.42.153'  # The server's hostname or IP address; post deployment, use laptop ip
 PORT = 5005        # The port used by the server
+
+# Serial config
+SERIAL_PORT = '/dev/ttyACM0'
+BAUDRATE = 9600
 
 with socket(AF_INET, SOCK_STREAM) as s:
     try:
         s.connect((HOST, PORT))
+        # ds.create_serial(SERIAL_PORT, BAUDRATE)
         while True:            
-            data = s.recv(4096)        
+            data = s.recv(4096)                 # Blocking
 
-            if data == b"('Key', 'BTN_MODE', 1)":
+            if data == b"('BTN_MODE', 1)":
                 s.send(b'kill_server')
+                print("stopping client")
                 break
             else:
                 s.send(b'working')
 
+
             print(data)
+            decoded_data = data.decode('utf-8').strip("()")
+            event, state = decoded_data.split(',') 
+            print(type(decoded_data), event, state)
+            # ds.forward_data(event, state)
+
     except KeyboardInterrupt:
         print("\nexiting")
