292eb13cb13d3f055904dbc45ee2f2a138095e00
usvmac <usvmcmaster@gmail.com>
2019-10-09 23:34:51 -0400

    Socket client used as a repeater to send data from server to arduino

--- python/comm/client.py
+++ python/comm/client.py
@@ -1,8 +1,9 @@
 from socket import socket, AF_INET, SOCK_STREAM
-# import controller.data_serializer as ds
+import controller.data_serializer as ds
+
 # Socket config
 HOST = '172.17.42.153'  # The server's hostname or IP address; post deployment, use laptop ip
-PORT = 5005        # The port used by the server
+PORT = 5005             # The port used by the server
 
 # Serial config
 SERIAL_PORT = '/dev/ttyACM0'
@@ -11,7 +12,7 @@ BAUDRATE = 9600
 with socket(AF_INET, SOCK_STREAM) as s:
     try:
         s.connect((HOST, PORT))
-        # ds.create_serial(SERIAL_PORT, BAUDRATE)
+        ser = ds.create_serial(SERIAL_PORT, BAUDRATE)
         while True:            
             data = s.recv(4096)                 # Blocking
 
@@ -27,7 +28,7 @@ with socket(AF_INET, SOCK_STREAM) as s:
             decoded_data = data.decode('utf-8').strip("()")
             event, state = decoded_data.split(',') 
             print(type(decoded_data), event, state)
-            # ds.forward_data(event, state)
+            ds.forward_state(ser, event, state)
 
     except KeyboardInterrupt:
         print("\nexiting")
