1f37026f15a20e1ae6853a395bb1191d13ddfc36
usvmac <usvmcmaster@gmail.com>
2019-10-10 01:13:00 -0400

    Completed initial thruster controller. Thurster code has been disabled for debugging.

--- arduino/ser2out/ser2out.ino
+++ arduino/ser2out/ser2out.ino
@@ -7,6 +7,8 @@
 void setup()
 {
   Serial.begin(9600);
+
+//  Temporarily disabled since motors are not connected
 //  thruster1.attach(t1Pin);
 //  thruster2.attach(t2Pin);
 //
@@ -24,10 +26,16 @@ void loop()
   int eventIndex;
   String event;
 
-  float state;  
-
+  float state;
   int signalValue;
-  
+
+  String t1_code = String("ABS_Y");
+  String t2_code = String("ABS_RY");
+
+//  t1 and t2 output values used only for serial monitor
+  int t1_output;
+  int t2_output;
+
   while (Serial.available())
   {
     if(Serial.available() > 0)
@@ -38,21 +46,34 @@ void loop()
       event = data.substring(0, eventIndex);
 
       state = (data.substring(eventIndex+1)).toFloat();
-      
-      
-//      Serial.print("Command:");
-//      Serial.print(command);
-//      
-//      Serial.print(" Value:");
-//      Serial.println(value);
-          
-      //1700; // Set signal value, which should be between 1100 and 1900
-      signalValue = (((1900-1500)*(state - 0))/(1-0)) + 1500;
 
-      //Serial.print("Signal Value");
-      Serial.println(signalValue);
-//      thruster1.writeMicroseconds(signalValue); // Send signal to ESC.
-//      thruster2.writeMicroseconds(signalValue);
-    }    
+//      (new_sig_max-new_sig_min)*(input - min)/(max-min) + offset
+      signalValue = ((1900-1500)*state) + 1500;
+
+//      The following if statements are an abomination.
+//      Please forgive me.
+      if (event == t1_code)
+      {
+//        thruster1.writeMicroseconds(signalValue);
+        t1_output = signalValue;
+      }
+
+      else if (event == t2_code)
+      {
+//        thruster2.writeMicroseconds(signalValue);
+        t2_output = signalValue;
+      }
+
+      if ((event == t1_code) || (event == t2_code))
+      {
+        Serial.print("T1 Output:");
+        Serial.print(" ");
+        Serial.print(t1_output);
+
+        Serial.print(" | T2 Output:");
+        Serial.print(" ");
+        Serial.println(t2_output);
+      }
+    }
   }
 }
