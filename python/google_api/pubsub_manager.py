22e47a5c0027976f69b1ae1f5112e5b99b41fdac
rohit <rawatr@mcmaster.ca>
2019-11-05 11:41:22 -0500

    Combining pub/sub

---
+++ python/google_api/pubsub_manager.py
@@ -0,0 +1,30 @@
+import os
+from google.cloud import pubsub_v1
+
+def callback(message):
+    print(message.data)
+    message.ack()
+
+if __name__ == "__main__":
+    project_id='capstone-1554124676827'
+    topic_name='usv_data'
+
+    publisher = pubsub_v1.PublisherClient()
+    project_path = publisher.project_path(project_id)
+    topic_path = publisher.topic_path(project_id, topic_name)
+
+    for topic in publisher.list_topics(project_path):
+        print(topic)
+
+    subscription_name ='usv_data_subscriber'
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = subscriber.subscription_path(project_id, subscription_name)
+
+
+
+    future = subscriber.subscribe(subscription_path, callback)
+
+    try:
+        future.result()
+    except KeyboardInterrupt:
+        future.cancel()
