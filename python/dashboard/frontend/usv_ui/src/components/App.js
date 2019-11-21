1232744dc260d07d30e7102dddff578d782dc0fe
rohit <rawatr@mcmaster.ca>
2019-11-21 01:52:06 -0500

    Cleaned up UI and added buttons.

--- python/dashboard/frontend/usv_ui/src/components/App.js
+++ python/dashboard/frontend/usv_ui/src/components/App.js
@@ -5,35 +5,47 @@ import PaperSheet from './templates/bodyLayout';
 
 
 class App extends Component {
-  
+
   componentDidMount() {
     this.renderMap()
   }
-  
+
   initMap = () => {
     var marker_count = 0;
     var markers = [];
 
+    var click_timeout = null;
+
     // eslint-disable-next-line
     var map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 43.266951, lng: -79.921734},
+      center: { lat: 43.266951, lng: -79.921734 },
       zoom: 15
     });
 
-    function addMarker (latLng, map) {
+    function addMarker(latLng, map) {
       // eslint-disable-next-line
       markers[marker_count] = new window.google.maps.Marker({
-      position: {lat: latLng.lat(), lng:latLng.lng()},
-      map: map
+        position: { lat: latLng.lat(), lng: latLng.lng() },
+        map: map
       });
 
       marker_count++
+      console.log(marker_count)
+      console.log(markers[marker_count])
     }
-    
-    window.google.maps.event.addListener(map, 'click', function(event) {
+
+    // Handler for single click event
+    window.google.maps.event.addListener(map, 'click', function (event) {
       // console.log(event.latLng.lat())
       // console.log(event.latLng.lng())
-      addMarker(event.latLng, map) //<-- adds new marker per function call
+      click_timeout = setTimeout(function () {
+        addMarker(event.latLng, map) //<-- adds new marker per function call
+      }, 200);
+    });
+
+    // Handler for double click event (ensures double click doesn't place marker)
+    window.google.maps.event.addListener(map, 'dblclick', function (event) {
+      clearTimeout(click_timeout);
     });
   }
 
@@ -41,24 +53,24 @@ class App extends Component {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDdjnJdmnyoNX2btE-w8MHDdeTPhQgb6cs&callback=initMap")
     window.initMap = this.initMap
   }
-  
+
   render() {
     return (
       <main>
-        <ButtonAppBar/>
+        <ButtonAppBar />
         {/* <div id="map"></div> */}
         <PaperSheet></PaperSheet>
       </main>
     );
-  } 
+  }
 }
 
 
 function loadScript(url) {
   // First element of all elements with tag name script
-  var index = window.document.getElementsByTagName("script")[0] 
+  var index = window.document.getElementsByTagName("script")[0]
   var script = window.document.createElement("script")
-  
+
   script.src = url
   script.async = true
   script.defer = true
--- python/dashboard/frontend/usv_ui/src/components/templates/bodyLayout.js
+++ python/dashboard/frontend/usv_ui/src/components/templates/bodyLayout.js
@@ -3,59 +3,61 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
-    // root == container level
-    root: {
-        flexGrow: 1,
-        padding: 20,
-        // height,
-        // marginTop:20
-        
-        
-      },
-      // control == item level
-      itembox: {
-        // height: 100,
-        // width: 100,
-        // margin: 5
-      },
+  // root == container level
+  root: {
+    flexGrow: 1,
+    padding: 20,
+    // height,
+    // marginTop:20
 
-      paper: {
-        padding: theme.spacing(2),
-        height: 400,
-        width: 350
-      },
 
-      button: {
-        padding: theme.spacing(2),
-      },
-  }));
+  },
+  // control == item level
+  itembox: {
+    // height: 100,
+    // width: 100,
+    // margin: 50
+  },
+
+  mapPaper: {
+    padding: theme.spacing(2),
+    height: 500,
+    // width: "100%"
+  },
+
+  buttonContainer: {
+
+  },
+
+  button: {
+    padding: theme.spacing(2),
+    // margin: 10,
+    marginBottom: theme.spacing(2),
+    width: 165
+  },
+}));
 
 export default function PaperSheet() {
-    const classes = useStyles();
-    
-    return (
-        <Grid container className={classes.root}>
-            <Grid item className={classes.itembox}>
-                <Paper className={classes.paper}>
-                    <div id="map"></div>
-                    <Button variant="contained" color="primary" className={classes.button}>Set Start Point</Button>
-                    <Button variant="contained" color="primary" className={classes.button}>Set End Point</Button>
-                    <Button variant="contained" color="primary" className={classes.button}>Reset</Button>
-                </Paper>
-                {/* <Paper>Button</Paper> */}
-            </Grid>
+  const classes = useStyles();
 
-            <Grid item className={classes.itembox}>
-                <Paper className={classes.paper}>
-                    <Typography variant="h4">
-                        Coordinates go here!
-                    </Typography>
-                </Paper>
-            </Grid>
+  return (
+
+    <Grid container className={classes.root}>
+      <Grid container justify={"space-around"} alignItems={"center"} className={classes.buttonContainer}>
+          <Button variant="outlined" color="primary" className={classes.button}>Set Start Point</Button>
+          <Button variant="outlined" color="primary" className={classes.button}>Set End Point</Button>
+          <Button variant="outlined" color="primary" className={classes.button}>Reset</Button>
         </Grid>
-    )
+        
+        <Grid item xs>
+          <Paper className={classes.mapPaper}>
+            <div id="map"></div>
+          </Paper>
+        </Grid>
+    </Grid>
+  )
 }
