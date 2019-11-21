8f94742a00d30adbc181fdd156b74e126103b8d8
rohit <rawatr@mcmaster.ca>
2019-11-21 18:21:48 -0500

    Combined snacks and bodyLayout. TODO: Remove snacks

--- python/dashboard/frontend/usv_ui/src/components/templates/Snacks.js
+++ python/dashboard/frontend/usv_ui/src/components/templates/Snacks.js
@@ -5,81 +5,81 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles(theme => ({
-    close: {
-        padding: theme.spacing(0.5),
-    },
+  close: {
+    padding: theme.spacing(0.5),
+  },
 }));
 
 export default function Snacks() {
 
-    // Following functions from material-ui guide
-    const queueRef = React.useRef([]);
-    const [open, setOpen] = React.useState(false);
-    const [messageInfo, setMessageInfo] = React.useState(undefined);
-  
-    const processQueue = () => {
-      if (queueRef.current.length > 0) {
-        setMessageInfo(queueRef.current.shift());
-        setOpen(true);
-      }
-    };
-  
-    const handleClick = message => () => {
-      queueRef.current.push({
-        message,
-        key: new Date().getTime(),
-      });
-  
-      if (open) {
-        // immediately begin dismissing current message
-        // to start showing new one
-        setOpen(false);
-      } else {
-        processQueue();
-      }
-    };
-  
-    const handleClose = (event, reason) => {
-      if (reason === 'clickaway') {
-        return;
-      }
+  // Following functions from material-ui guide
+  const queueRef = React.useRef([]);
+  const [open, setOpen] = React.useState(false);
+  const [messageInfo, setMessageInfo] = React.useState(undefined);
+
+  const processQueue = () => {
+    if (queueRef.current.length > 0) {
+      setMessageInfo(queueRef.current.shift());
+      setOpen(true);
+    }
+  };
+
+  const handleClick = message => () => {
+    queueRef.current.push({
+      message,
+      key: new Date().getTime(),
+    });
+
+    if (open) {
+      // immediately begin dismissing current message
+      // to start showing new one
       setOpen(false);
-    };
-  
-    const handleExited = () => {
+    } else {
       processQueue();
-    };
+    }
+  };
 
-    const classes = useStyles();
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
 
-    return (
-        <div>
-            <Snackbar
-                key={messageInfo ? messageInfo.key : undefined}>
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-                open={open}
-                autoHideDuration={6000}
-                onClose={handleClose}
-                onExited={handleExited}
-                ContentProps={{
-                    'aria-describedby': 'message-id',
-                }}
-                message={<span id="message-id">{messageInfo ? messageInfo.message : undefined}</span>}
-                action={[
-                    <IconButton
-                        key="close"
-                        aria-label="close"
-                        color="inherit"
-                        className={classes.close}
-                        onClick={handleClose}
-                    >
-                        <CloseIcon />
-                    </IconButton>
-                ]}
-            </Snackbar>
-        </div>
-    );
+  const handleExited = () => {
+    processQueue();
+  };
+
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Snackbar
+        key={messageInfo ? messageInfo.key : undefined}>
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        onExited={handleExited}
+        ContentProps={{
+          'aria-describedby': 'message-id',
+        }}
+        message={<span id="message-id">{messageInfo ? messageInfo.message : undefined}</span>}
+        action={[
+          <IconButton
+            key="close"
+            aria-label="close"
+            color="inherit"
+            className={classes.close}
+            onClick={handleClose}
+          >
+            <CloseIcon />
+          </IconButton>
+        ]}
+      </Snackbar>
+    </div>
+  );
 }
--- python/dashboard/frontend/usv_ui/src/components/templates/bodyLayout.js
+++ python/dashboard/frontend/usv_ui/src/components/templates/bodyLayout.js
@@ -1,63 +1,171 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
+// Main Body
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-// import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip'
+
+// Snacks
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles(theme => ({
   // root == container level
   root: {
     flexGrow: 1,
     padding: 20,
-    // height,
-    // marginTop:20
-
-
-  },
-  // control == item level
-  itembox: {
-    // height: 100,
-    // width: 100,
-    // margin: 50
   },
 
   mapPaper: {
     padding: theme.spacing(2),
     height: 500,
-    // width: "100%"
-  },
-
-  buttonContainer: {
-
   },
 
   button: {
     padding: theme.spacing(2),
-    // margin: 10,
     marginBottom: theme.spacing(2),
     width: 165
   },
+
+  // Tooltip
+  absolute: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(3),
+  },
+
+  // Snack
+  close: {
+    padding: theme.spacing(0.5),
+  },
 }));
 
-export default function PaperSheet() {
+export default function ConsecutiveSnackbars() {
+  const queueRef = React.useRef([]);
+  const [open, setOpen] = React.useState(false);
+  const [messageInfo, setMessageInfo] = React.useState(undefined);
+
+  const processQueue = () => {
+    if (queueRef.current.length > 0) {
+      setMessageInfo(queueRef.current.shift());
+      setOpen(true);
+    }
+  };
+
+  // eslint-disable-next-line
+  const handleClick = message => () => {
+    queueRef.current.push({
+      message,
+      key: new Date().getTime(),
+    });
+
+    if (open) {
+      // immediately begin dismissing current message
+      // to start showing new one
+      setOpen(false);
+    } else {
+      processQueue();
+    }
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const handleExited = () => {
+    processQueue();
+  };
+
   const classes = useStyles();
-
   return (
+    <div>
+      <Grid container className={classes.root}>
+        <Grid container justify={"space-around"} alignItems={"center"} className={classes.buttonContainer}>
+          <Tooltip title="Click on the map after clicking this button to set the start point" placement="bottom">
+            <Button
+              id="btn_start"
+              variant="outlined"
+              color="primary"
+              // onClick={handleClick('Click on the map to register the start point')}
+              className={classes.button}>Set Start Point
+            </Button>
+          </Tooltip>
 
-    <Grid container className={classes.root}>
-      <Grid container justify={"space-around"} alignItems={"center"} className={classes.buttonContainer}>
-          <Button variant="outlined" color="primary" className={classes.button}>Set Start Point</Button>
-          <Button variant="outlined" color="primary" className={classes.button}>Set End Point</Button>
-          <Button variant="outlined" color="primary" className={classes.button}>Reset</Button>
+          <Tooltip title="Click on the map after clicking this button to set the end point" placement="bottom">
+            <Button
+              id="btn_end"
+              variant="outlined"
+              color="primary"
+              // onClick={handleClick('Click on the map to register the end point')}
+              className={classes.button}>Set End Point
+           </Button>
+          </Tooltip>
+
+          <Tooltip title="Click this button to generate a path between the start and end point" placement="bottom">
+            <Button
+              id="btn_generate"
+              variant="outlined"
+              color="primary"
+              // onClick={handleClick('Test3')}
+              className={classes.button}>Generate Path
+           </Button>
+          </Tooltip>
+
+          <Tooltip title="Click this button to remove all markers" placement="bottom">
+            <Button
+              id="btn_reset"
+              variant="outlined"
+              color="primary"
+              // onClick={handleClick('Test4')}
+              className={classes.button}>Reset
+            </Button>
+          </Tooltip>
+
         </Grid>
-        
-        <Grid item xs>
-          <Paper className={classes.mapPaper}>
-            <div id="map"></div>
-          </Paper>
-        </Grid>
-    </Grid>
-  )
+      </Grid>
+
+      <Grid item xs>
+        <Paper className={classes.mapPaper}>
+          <div id="map"></div>
+        </Paper>
+      </Grid>
+
+      {/* Snackbar handler */}
+      <Snackbar
+        key={messageInfo ? messageInfo.key : undefined}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        onExited={handleExited}
+        ContentProps={{
+          'aria-describedby': 'message-id',
+        }}
+        message={<span id="message-id">{messageInfo ? messageInfo.message : undefined}</span>}
+        action={[
+          <Button key="undo" color="secondary" size="small" onClick={handleClose}>
+            UNDO
+          </Button>,
+          <IconButton
+            key="close"
+            aria-label="close"
+            color="inherit"
+            className={classes.close}
+            onClick={handleClose}
+          >
+            <CloseIcon />
+          </IconButton>,
+        ]}
+      />
+    </div>
+  );
 }
